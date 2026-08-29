1fd01c43f0101b19e1d17ef49d2d586de3b3e6dc
porhe911 <114869553+porhe911@users.noreply.github.com>
2026-08-29 15:56:32 +0300

    stageB: declare embedded ShellUI renderer blob

---
+++ src/ps5/embedded_renderer.hpp
@@ -0,0 +1,16 @@
+/*
+ * Common FPS for PS5
+ * Copyright (C) 2026 porhe911
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+#include <cstddef>
+
+namespace common_fps::ps5::embedded_renderer {
+
+extern const unsigned char kData[];
+extern const std::size_t kSize;
+
+} // namespace common_fps::ps5::embedded_renderer
